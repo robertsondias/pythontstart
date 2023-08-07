be922c3c9550e94feb882178df793ccd7428f9a7
robertsondias <robertsondias@gmail.com>
2023-08-07 10:14:01 -0300

    Add funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,21 +1,24 @@
-# esse é um parâmetro. O parâmetro é a VARIÁVEL "OPERAÇÃO"
+# esse é um parâmetro. O parâmetro é a VARIÁVEL "OPERAÇÃO". Parametro é quando modera a função
 
+nome = "python"
 def soma_multiplicacao(valor1, valor2, operacao):
     if operacao == "somar":
         resultado = valor1 + valor2
     if operacao == "multiplicar":
         resultado = valor1 * valor2
     return resultado
+print(nome)
+
     
-valor1 = float(input("passe o primeiro valor: "))
-valor2 = float(input("passe o segunto valor: "))
+valor_1 = float(input("passe o primeiro valor: "))
+valor_2 = float(input("passe o segunto valor: "))
 opcao = input("Qual operação será feita?")
-resultado = soma_multiplicacao(valor1, valor2, opcao)
+resultado = soma_multiplicacao(valor_1, valor_2, opcao)
 print(resultado, "opcao escolhida é", opcao)
 
+# def soma(a,b):  # é argumentos, pois foram informações utilizadas para geradar resultado. 
+#É quando insere informações p/ ajudar na função.
 
-
-# def soma(a,b):  # é argumentos, pois foram informações utilizadas para geradar resultado
 #     resultado = a + b
 #     return resultado
 
