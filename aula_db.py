22d1349526e7005860af8f3de1ae54744e7a64c0
robertsondias <robertsondias@gmail.com>
2023-08-19 16:28:16 -0300

    Update

--- aula_db.py
+++ aula_db.py
@@ -1,33 +1,34 @@
 import sqlite3
 
-conexao = sqlite3.connect("aula.db")
+conexao = sqlite3.connect("aula_db")
 
 conexao.execute ('''CREATE TABLE IF NOT EXISTS aluno
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
                  nome TEXT NOT NULL,
                  idade INT NOT NULL);''')
 
-def cria_aluno(nome, idade):
-    conexao.execute("INSERT INTO aluno (nome, idade) VALUES (?, ?);", (nome, idade));
+def create_aluno(nome, idade):
+    conexao.execute("INSERT INTO aluno (nome, idade) VALUES (?, ?);", (nome, idade))
     conexao.commit()
-    print("Aluno cadastrado")
+    print("Aluno cadastrado com sucesso")
     
-def listar_alunos():
+def list_aluno():
     alunos = conexao.execute("SELECT * FROM aluno;")
     for aluno in alunos:
         print(aluno)
-    
-def atualizar_aluno(id, novo_nome, nova_idade):
-    conexao.execute("UPDATE aluno SET nome = ?, idade = ? WHERE id = ?;", (novo_nome, nova_idade, id))
+        
+def update_aluno(id, novo_nome, nova_idade):
+    conexao.execute("UPDATE aluno SET nome = ?, idade = ? WHERE id = ?;", (novo_nome, nova_idade,id))
     conexao.commit()
     print("Dados atualizados corretamente")
     
-def deletar_aluno(id):
+def delete_aluno(id):
     conexao.execute("DELETE FROM aluno WHERE id = ?;",(id,))
     conexao.commit()
-    print("Arquivo deletado com sucesso")
+    print("Aluno deletado com sucesso")
     
-#cria_aluno("Elza", "79")
-# atualizar_aluno(4,"Sylvia","30")
-deletar_aluno(6)
-listar_alunos()
+#create_aluno("Tokio", "3")
+#update_aluno(3, "Sylvia", "30")
+delete_aluno(5)
+list_aluno()
+    
